f9c6380f13134208d51e9b4f86d72fd03a39448f
Dragos Neacsu <neacsudragos22@gmail.com>
2017-08-23 12:40:48 +0100

    Add translation for all words

--- FakeTicket/App_LocalResources/GlobalRes.Designer.cs
+++ FakeTicket/App_LocalResources/GlobalRes.Designer.cs
@@ -61,11 +61,146 @@ namespace FakeTicket.App_LocalResources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Airline.
+        /// </summary>
+        public static string Airline {
+            get {
+                return ResourceManager.GetString("Airline", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Boarding Time.
+        /// </summary>
+        public static string BoardingTime {
+            get {
+                return ResourceManager.GetString("BoardingTime", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Business.
+        /// </summary>
+        public static string Business {
+            get {
+                return ResourceManager.GetString("Business", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Class.
+        /// </summary>
+        public static string Class {
+            get {
+                return ResourceManager.GetString("Class", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Depart from.
+        /// </summary>
+        public static string DepartFrom {
+            get {
+                return ResourceManager.GetString("DepartFrom", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Departure date.
+        /// </summary>
+        public static string DepartureDate {
+            get {
+                return ResourceManager.GetString("DepartureDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Destination.
+        /// </summary>
+        public static string Destination {
+            get {
+                return ResourceManager.GetString("Destination", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Economy.
+        /// </summary>
+        public static string Economy {
+            get {
+                return ResourceManager.GetString("Economy", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to First Class.
+        /// </summary>
+        public static string FirstClass {
+            get {
+                return ResourceManager.GetString("FirstClass", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to First name.
         /// </summary>
         public static string FirstName {
             get {
                 return ResourceManager.GetString("FirstName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Flight number.
+        /// </summary>
+        public static string FlightNumber {
+            get {
+                return ResourceManager.GetString("FlightNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gate.
+        /// </summary>
+        public static string Gate {
+            get {
+                return ResourceManager.GetString("Gate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Last name.
+        /// </summary>
+        public static string LastName {
+            get {
+                return ResourceManager.GetString("LastName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Premium Economy.
+        /// </summary>
+        public static string PremiumEconomy {
+            get {
+                return ResourceManager.GetString("PremiumEconomy", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Seat.
+        /// </summary>
+        public static string Seat {
+            get {
+                return ResourceManager.GetString("Seat", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Submit.
+        /// </summary>
+        public static string Submit {
+            get {
+                return ResourceManager.GetString("Submit", resourceCulture);
             }
         }
     }
